e27ae657b2732a1d88c1a3a4e31ae56df7cfa2aa
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-11-08 13:49:14 +0100

    Prevent navbar from overlapping page content

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,12 @@
+import './layout.css';
+
 import * as React from 'react';
 import { Container } from 'reactstrap';
 import Navbar from './navbar';
 
 const Layout = (props: { children?: React.ReactNode }) =>
   <div>
-    <Container fluid={true}>
+    <Container fluid={true} id="content">
       {props.children}
     </Container>
     <Navbar />
