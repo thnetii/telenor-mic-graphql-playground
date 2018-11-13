4e58bb9cd791556b046b01c77df82149331d5ffa
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-11-13 20:52:45 +0100

    MicHostConnectionForm

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 
-const PageHeader = () =>
-  <React.Fragment>
-    <h1 className='display-4'>
-      Telenor Connexion Managed IoT Cloud
-      <br />
-      <small className='text-muted'>Interactive GraphQL Playground</small>
-    </h1>
-  </React.Fragment>;
+export class PageHeader extends React.PureComponent {
+  public render() {
+    return (
+      <React.Fragment>
+        <h1 className='display-4'>
+          Telenor Connexion Managed IoT Cloud
+          <br />
+          <small className='text-muted'>Interactive GraphQL Playground</small>
+        </h1>
+      </React.Fragment>
+    );
+  }
+}
 
 export default PageHeader;
